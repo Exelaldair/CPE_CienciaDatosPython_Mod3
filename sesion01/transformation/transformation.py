299ca49f0b5d8b4c4151de91d3daa86497e834b5
jgomezm-tecsup <jgomezm@tecsup.edu.pe>
2019-09-22 12:07:10 -0500

    Sesion 01

---
+++ sesion01/transformation/transformation.py
@@ -0,0 +1,10 @@
+# import pandas library
+import pandas as pd
+
+headers = ["symboling","normalized-losses","make","fuel-type","aspiration", "num-of-doors","body-style",
+         "drive-wheels","engine-location","wheel-base", "length","width","height","curb-weight","engine-type",
+         "num-of-cylinders", "engine-size","fuel-system","bore","stroke","compression-ratio","horsepower",
+         "peak-rpm","city-mpg","highway-mpg","price"]
+
+file_path = "auto.csv"
+df = pd.read_csv(file_path, names=headers)
