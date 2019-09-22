90efff71cebedee0ba30c4bedfaa05e683be4772
jgomezm-tecsup <jgomezm@tecsup.edu.pe>
2019-09-22 09:26:39 -0500

    Sesion 01

--- sesion01/web_scraping/request_read_url_basic.py
+++ sesion01/web_scraping/request_read_url_basic.py
@@ -1,9 +1,11 @@
 '''
-
-library requests
-
+Programa : Ciencia de Datos con Pthon
+Modulo 03 : Estadística y Visualización de Datos con Python
+Sesion 01 : Analisis de Datos con Python - Web Scraping
+Fecha : 22/09/2019
+Version : 1
+Author : Jaime Gomez
 '''
-
 
 import requests
 
