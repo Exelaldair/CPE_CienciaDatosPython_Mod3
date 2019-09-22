ac64dcb60582137e5bfa86aa117f50af3ddea415
jgomezm-tecsup <jgomezm@tecsup.edu.pe>
2019-09-22 12:09:23 -0500

    Sesion 01

--- sesion01/transformation/transformation.py
+++ sesion01/transformation/transformation.py
@@ -8,3 +8,25 @@ headers = ["symboling","normalized-losses","make","fuel-type","aspiration", "num
 
 file_path = "auto.csv"
 df = pd.read_csv(file_path, names=headers)
+
+print(df.describe().round(2))
+
+price = df['price'].loc[df['price'] !='?'].count()
+print(price)
+
+
+import numpy as np
+
+# replace "?" to NaN
+df.replace("?", np.nan, inplace = True)
+df.head(5)
+
+#
+missing_data = df.isnull()
+print(missing_data.head(5))
+
+
+for column in missing_data.columns.values.tolist():
+    print(column)
+    print (missing_data[column].value_counts())
+    print("")
